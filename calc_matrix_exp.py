a1cf2bcbc2e5caee966ff392d1a93ae96912f78a
danieljsharpe <daniel.j.sharpe1995@gmail.com>
2019-04-23 18:18:45 +0100

    Multi-level coarsening appears to work, added use of Python interpreter

---
+++ calc_matrix_exp.py
@@ -0,0 +1,12 @@
+'''
+Calculate the matrix exponential at a given lag time. For use with MLR-MCL algorithm C++ code.
+
+Daniel J. Sharpe, April 2019
+'''
+
+# import numpy as np
+# from scipy.linalg import expm
+
+# calculate the exponential of the matrix A
+def calc_expm(A):
+    print "inside calc_matrix_exp!\n"
